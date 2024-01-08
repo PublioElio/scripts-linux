a926c3953183b4b9f27151c4d26937b1c5a725d3
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-08 12:40:17 +0100

    modificación script02

--- practica-scripts01/pscript02.sh
+++ practica-scripts01/pscript02.sh
@@ -6,7 +6,4 @@ do
 	whoami;cd;ls -l
 	echo "¿quieres ejecutar de nuevo el script? (s/n)"
 	read -r var
-	if [ -z "$var" ] || [ "$var" != "s" ] && [ "$var" != "n" ]; then
-		echo "Introduzca un valor válido (s/n)"
-	fi
 done
