31dbfa825ce75966c7407f66f3b001a15893496b
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-17 17:39:29 +0100

    script5

--- practica-scripts01/pscript05.sh
+++ practica-scripts01/pscript05.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo -n "Ingrese la ruta completa del archivo: ";read -r file
+
+if [ -r "$file" ]; then
+    read -p "Ingrese la ruta completa del directorio de destino: " dir
+    cp "$file" "$dir"
+    echo "Archivo copiado con éxito."
+else
+    echo "No se puede leer el archivo. Verifica la ruta y los permisos."
+fi
+
