d512abd5c17cfa5178309bb44e316fb9bd589f15
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-20 21:13:06 +0100

    script 17

---
+++ practica-scripts01/pscript17_0.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+variable0="Esta variable fue creada en el script $0"
+variable1=42
+export variable0;export variable1
+./pscript17_1.sh
---
+++ practica-scripts01/pscript17_1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Este es el $0"
+echo "Primera variable recibida: '$variable0'"
+echo "Segunda variable recibida: '$variable1'"
