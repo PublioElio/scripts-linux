d63adc7068ca5e0e735f631937f2c2fb94dbbf88
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-22 12:57:11 +0100

    examen scripts

---
+++ examen_hlc_adiaz/ejercicio01.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+factorial() {
+    if [ "$1" -eq 0 ] || [ "$1" -eq 1 ]; then
+        echo 1
+    else
+        echo $(( "$1" * $(factorial $(( "$1" - 1 ))) ))
+    fi
+}
+
+calc_descuento() {
+    porcentaje=$(echo "scale=2; $2 / 100" | bc)
+    a=$(echo "scale=2; $1 - ($1 * $porcentaje)"| bc)
+    echo $a   
+}
+
+contador=0
+while true
+do
+    
+    echo -e "a) Factorial\nb) Descuento\nc) Salida"
+    read -p "Elija una opción (a/b/c): " opcion
+
+    case "$opcion" in
+        a)
+        contador=$(($contador + 1))
+        input=""
+        echo "Ha elegido la opción 'factorial'"
+        while [[ ! "$input" =~ ^[0-9]+$ ]]
+        do
+            read -p "Introduzca un número: " input
+        done
+        clear
+        echo "El factorial de $input es $(factorial "$input")"   
+        ;;
+        b)
+        contador=$(($contador + 1))
+        descuento=1
+        clear
+        echo "Ha elegido la opción 'Descuento'"
+        read -p "Introduzca un valor para la prenda: " prenda
+        while [ "$descuento" -lt 10 ] || [ "$descuento" -gt 50 ]
+        do
+            read -p "Introduzca un descuento válido(10/50): " descuento
+        done
+        clear
+        echo "El $descuento% de '$prenda' es: $(calc_descuento "$prenda" "$descuento")"
+        ;;
+        c)
+        contador=$(($contador + 1))
+        clear
+        read -p "¿Está seguro de que desea salir? (s/n): " salir
+        if [ $salir == "s" ]; then
+            echo "Total de opciones seleccionadas: '$contador'. Finalizando el programa";exit 1
+        fi
+        ;;
+        *)
+        echo "entrada no válida"
+        ;;
+    esac
+done
---
+++ examen_hlc_adiaz/ejercicio02.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+aciertos=0
+partidas=0
+while true
+do
+    
+    read -p "Introduzca un fichero o directorio válido: " input
+
+    if [ -d "$input" ]; then
+        partidas=$(($partidas + 1))
+        total_archivos=$(ls $input | wc -l)
+        i=0
+        while [ "$i" -lt 2 ]
+        do
+            read -p "¿Cuántos archivos tiene el directorio? " archivos
+            if [ "$archivos" == "$total_archivos" ]; then
+                clear
+                echo "¡Acertaste! el directorio "$input" tiene "$archivos" archivos"
+                i=3
+                aciertos=$(($aciertos + 1))
+            else
+                i=$(($i + 1))
+                echo "No has acertado. Número de intentos que te quedan: $((2 - "$i"))"
+            fi
+        done
+    fi
+
+    if [ -f "$input" ]; then
+        partidas=$(($partidas + 1))
+        total_lineas=$(($(cat "$input" | wc -l) + 1))
+        i=0
+        while [ "$i" -lt 2 ]
+        do
+            read -p "¿Cuántas líneas tiene el fichero? " lineas
+            if [ "$lineas" == "$total_lineas" ]; then
+                clear
+                echo "¡Acertaste! el fichero "$input" tiene "$lineas" líneas"
+                i=3
+                aciertos=$(($aciertos + 1))
+            else
+                i=$(($i + 1))
+                echo "No has acertado. Número de intentos que te quedan: $((2 - "$i"))"
+            fi
+        done
+    fi
+
+    if [ "$partidas" -ge 1 ]; then
+        read -p "¿Quiere salir del juego? (s/n) " salir
+        if [ "$salir" == "s" ]; then
+            clear
+            echo -e "Finalizando el juego.\nTotal de aciertos: "$aciertos"\nTotal partidas "$partidas"";exit 1
+        fi
+    fi
+
+done
